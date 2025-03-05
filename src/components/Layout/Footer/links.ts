47700ede91140cc6c8805bef3413f5c402bc740d
Jesse Lind <52766281+jclind@users.noreply.github.com>
2025-03-05 16:04:36 -0500

    feat(components): add placeholder logo and legal data, footer, hero and add ButtonLink and Divider components

---
+++ src/components/Layout/Footer/links.ts
@@ -0,0 +1,23 @@
+export const links = [
+  {
+    title: 'Home',
+    path: '/',
+  },
+  {
+    title: 'About',
+    path: '/about',
+  },
+
+  {
+    title: 'Web Design',
+    path: '/web-design',
+  },
+  {
+    title: 'Pricing',
+    path: '/pricing',
+  },
+  {
+    title: 'Contact',
+    path: '/contact',
+  },
+]
---
+++ src/components/Layout/Navbar/navData.ts
@@ -0,0 +1,22 @@
+export const navLinks = [
+  {
+    title: 'Home',
+    path: '/',
+  },
+  {
+    title: 'About',
+    path: '/about',
+  },
+  {
+    title: 'Web Design',
+    path: '/web-design',
+  },
+  {
+    title: 'Pricing',
+    path: '/pricing',
+  },
+  {
+    title: 'Contact',
+    path: '/contact',
+  },
+]
